1df00014feb738d4fbac3faf7baec65c2b22b23a
Rajni315 <63162612+Rajni315@users.noreply.github.com>
2020-08-05 10:29:43 +0500

    Add files via upload

---
+++ asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for NurtureHealth project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'NurtureHealth.settings')
+
+application = get_asgi_application()
